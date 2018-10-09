f0558549b78dbe6b884824db682211600a6f0096
demitry2k <43990510+demitry2k@users.noreply.github.com>
2018-10-09 16:17:40 +0500

    Add files via upload

---
+++ Program2.pas
@@ -0,0 +1,7 @@
+﻿program j;
+var a,b:integer;
+begin
+writeln ('Введите длины двух катетов');
+readln(a,b);
+writeln('Гипотенуза равна  ',Sqrt(a*a+b*b))
+end.
---
+++ Program3.pas
@@ -0,0 +1,7 @@
+﻿program j;
+var x:integer;
+begin
+writeln ('Введите значение x');
+readln(x);
+writeln('y=',(x*x-sin(x)+cos(x))*3.1415926535/180)
+end.
